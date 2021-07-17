6b8312ec9b0e4e96222f8c34c4664bf0af928c54
Hanif Izzudin Rahman <47806867+hanifizzudinrahman@users.noreply.github.com>
2021-07-17 10:41:38 +0700

    Add files via upload

---
+++ 04.0 Calibration 4 Camera - Step by Step -from PC-/2.6.2 - Get Pixel to Crop SETTING MANUAL.py
@@ -0,0 +1,15 @@
+# Crop Coordinate
+CropKriAts = np.zeros((2), np.int)
+CropKanAts = np.zeros((2), np.int)
+CropKanBwh = np.zeros((2), np.int)
+CropKriBwh = np.zeros((2), np.int)
+
+CropKriAts = [33, 32]
+CropKanAts = [475, 33]
+CropKanBwh = [476, 634]
+CropKriBwh = [7, 631]
+
+print("CropKriAts: ", CropKriAts)
+print("CropKanAts: ", CropKanAts)
+print("CropKanBwh: ", CropKanBwh)
+print("CropKriBwh: ", CropKriBwh)
