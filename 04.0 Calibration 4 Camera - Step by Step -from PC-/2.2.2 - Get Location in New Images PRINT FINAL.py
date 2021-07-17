768262603843ed97025c436ef45a08e30287761b
Hanif Izzudin Rahman <47806867+hanifizzudinrahman@users.noreply.github.com>
2021-07-17 10:39:42 +0700

    Add files via upload

---
+++ 04.0 Calibration 4 Camera - Step by Step -from PC-/2.2.2 - Get Location in New Images PRINT FINAL.py
@@ -0,0 +1,45 @@
+#====================================================================================
+# FINAL
+print("FINAL Best Coordinate")
+print("~"*50)
+print()
+
+# Coordinate Kiri Atas
+print("Coordinate Kiri Atas")
+print("Take from Camera A, X1 = ", saveNameX1, " and Y1 = ", saveNameY1)
+print("=-"*25)
+print('x1A,y1A = ', corKriAts)
+print('x1B,y1B = ', corKriAtsB)
+print('x1C,y1C = ', corKriAtsC)
+print('x1D,y1D = ', corKriAtsD)
+print()
+
+# Coordinate Kanan Atas
+print("Coordinate Kanan Atas")
+print("Take from Camera A, X2 = ", saveNameX2, " and Y2 = ", saveNameY2)
+print("=-"*25)
+print('x2A,y2A = ', corKanAts)
+print('x2B,y2B = ', corKanAtsB)
+print('x2C,y2C = ', corKanAtsC)
+print('x2D,y2D = ', corKanAtsD)
+print()
+
+# Coordinate Kanan Bawah
+print("Coordinate Kanan Bawah")
+print("Take from Camera A, X3 = ", saveNameX3, " and Y3 = ", saveNameY3)
+print("=-"*25)
+print('x3A,y3A = ', corKanBwh)
+print('x3B,y3B = ', corKanBwhB)
+print('x3C,y3C = ', corKanBwhC)
+print('x3D,y3D = ', corKanBwhD)
+print()
+
+# Coordinate Kiri Bawah
+print("Coordinate Kiri Bawah")
+print("Take from Camera A, X4 = ", saveNameX4, " and Y4 = ", saveNameY4)
+print("=-"*25)
+print('x4A,y4A = ', corKriBwh)
+print('x4B,y4B = ', corKriBwhB)
+print('x4C,y4C = ', corKriBwhC)
+print('x4D,y4D = ', corKriBwhD)
+print()
