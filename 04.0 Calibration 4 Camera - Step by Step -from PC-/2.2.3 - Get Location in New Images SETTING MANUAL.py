eeb1dfd361fdf447d81b473c121a480728a84d23
Hanif Izzudin Rahman <47806867+hanifizzudinrahman@users.noreply.github.com>
2021-07-17 10:40:02 +0700

    Add files via upload

---
+++ 04.0 Calibration 4 Camera - Step by Step -from PC-/2.2.3 - Get Location in New Images SETTING MANUAL.py
@@ -0,0 +1,79 @@
+#====================================================================================
+# FINAL
+print("FINAL Best Coordinate")
+print("~"*50)
+print()
+
+saveNameX1 = 3
+saveNameX2 = 2
+saveNameX3 = 4
+saveNameX4 = 3
+#======================
+saveNameY1 = 4
+saveNameY2 = 4
+saveNameY3 = 2
+saveNameY4 = 5
+
+# Coordinate Kiri Atas
+#==============================
+corKriAts = [[33, 65]]
+corKriAtsB = [[19, 38]]
+corKriAtsC = [[ 7, 58]]
+corKriAtsD = [[ 9, 45]]
+#==============================
+print("Coordinate Kiri Atas")
+print("Take from Camera A, X1 = ", saveNameX1, " and Y1 = ", saveNameY1)
+print("=-"*25)
+print('x1A,y1A = ', corKriAts)
+print('x1B,y1B = ', corKriAtsB)
+print('x1C,y1C = ', corKriAtsC)
+print('x1D,y1D = ', corKriAtsD)
+print()
+
+# Coordinate Kanan Atas
+#==============================
+corKanAts = [[471,  52]]
+corKanAtsB = [[461,  24]]
+corKanAtsC = [[461,  35]]
+corKanAtsD = [[455,  24]]
+#==============================
+print("Coordinate Kanan Atas")
+print("Take from Camera A, X2 = ", saveNameX2, " and Y2 = ", saveNameY2)
+print("=-"*25)
+print('x2A,y2A = ', corKanAts)
+print('x2B,y2B = ', corKanAtsB)
+print('x2C,y2C = ', corKanAtsC)
+print('x2D,y2D = ', corKanAtsD)
+print()
+
+# Coordinate Kanan Bawah
+#==============================
+corKanBwh = [[470, 638]]
+corKanBwhB = [[462, 602]]
+corKanBwhC = [[470, 622]]
+corKanBwhD = [[458, 603]]
+#==============================
+print("Coordinate Kanan Bawah")
+print("Take from Camera A, X3 = ", saveNameX3, " and Y3 = ", saveNameY3)
+print("=-"*25)
+print('x3A,y3A = ', corKanBwh)
+print('x3B,y3B = ', corKanBwhB)
+print('x3C,y3C = ', corKanBwhC)
+print('x3D,y3D = ', corKanBwhD)
+print()
+
+# Coordinate Kiri Bawah
+#==============================
+corKriBwh = [[ 18, 631]]
+corKriBwhB = [[ 16, 597]]
+corKriBwhC = [[ 16, 633]]
+corKriBwhD = [[ 14, 599]]
+#==============================
+print("Coordinate Kiri Bawah")
+print("Take from Camera A, X4 = ", saveNameX4, " and Y4 = ", saveNameY4)
+print("=-"*25)
+print('x4A,y4A = ', corKriBwh)
+print('x4B,y4B = ', corKriBwhB)
+print('x4C,y4C = ', corKriBwhC)
+print('x4D,y4D = ', corKriBwhD)
+print()
